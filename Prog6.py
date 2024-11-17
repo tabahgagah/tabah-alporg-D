991168253a65e65ccbcc396ceb7bc486858a9296
tabah penemuan <tabahtabah831@gmail.com>
2024-11-17 12:26:23 +0800

    Add files via upload

---
+++ Prog6.py
@@ -0,0 +1,13 @@
+# Maukan pengguna
+
+# Panjang dan lebar
+panjang = 26
+lebar = 24
+
+# Menghitung luas
+luas = panjang * lebar
+
+# Menampilkan hasil
+print("Panjang:", panjang)
+print("Lebar:", lebar)
+print("Luas persegi panjang adalah:", luas) 
