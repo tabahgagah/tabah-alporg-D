8389bb016b4d15fd4898bd26b11454ed3b22c9c0
tabah penemuan <tabahtabah831@gmail.com>
2024-12-08 00:58:46 +0800

    Add files via upload

---
+++ prog8.py.py
@@ -0,0 +1,35 @@
+[00.25, 8/12/2024] Jaelani Pti: a = 24
+b = 47
+
+#operasi kali X
+hasil = a * b 
+print("nilai dari a * b =",hasil)
+
+#operasi bagi :
+hasil = a / b 
+print("nilai dari a / b =",hasil)
+
+#operasi tambah +
+hasil = a + b 
+print("nilai dari a + b =",hasil)
+
+#operasi kurang -
+hasil = a - b 
+print("nilai dari a - b=",hasil)
+
+#operasi perpangkatan **
+hasil = a ** b 
+print("nilai dari a ** b=",hasil)
+
+#modulus (sisa bagi) %
+hasil = a % b 
+print("nilai dari a mod b=",hasil)
+
+#floor dividion //
+hasil = a // b 
+print("nilai dari a flor b=",hasil)
+[00.25, 8/12/2024] Jaelani Pti: Tinggal ganti nim belakang
+[00.25, 8/12/2024] Tabah Penemuan: Ga kamu pake pdf?
+[00.26, 8/12/2024] Jaelani Pti: Mksd
+[00.26, 8/12/2024] Jaelani Pti: Buat dulu di apk itu tinggal kmu copy
+[00.26, 8/12/2024] Jaelani Pti: File nya kmu 
