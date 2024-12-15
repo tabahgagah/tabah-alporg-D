ef873ae4122b254058977a41b1419654cb89d8a6
tabah penemuan <tabahtabah831@gmail.com>
2024-12-15 14:24:50 +0800

    Add files via upload

---
+++ prog9.pyy.py
@@ -0,0 +1,19 @@
+# suhu celcius
+# rumus konversi suhu
+# suhu celcius = 47
+# suhu dalam Fahrenhit = 98.6
+# suhu dalam kelvin = 310.15
+# suhu dalam reamur = 19.2
+
+# Suhu dalam celcius
+celcius = 47
+
+# rumus konversi suhu
+fahrenheit = (celcius * 9/5) + 32
+kelvin = celcius + 273.15
+reamur = celcius * 4/5
+
+# cetak nilai suhu
+print("suhu dalam fahrenheit:", fahrenheit)
+print("suhu dalam kelvin:", kelvin)
+print("suhu dalam reamur:", reamur)
